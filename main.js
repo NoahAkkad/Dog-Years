102232addaa828de1c39090465abbdf669433879
NoahAkkad <noah.akkad@gmail.com>
2022-08-10 16:31:09 +0200

    newfile

---
+++ main.js
@@ -0,0 +1,18 @@
+let myName ='Noah';
+// variable My Age 
+let myAge= 41;
+// variable earlyYears 
+let earlyYears = 2;
+//
+earlyYears *= 10.5; 
+//
+let laterYears = myAge - 2;
+//
+laterYears *= 4;
+//
+console.log(earlyYears);
+//
+let myAgeInDogYears = earlyYears + laterYears ;
+//
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`.toLowerCase());
